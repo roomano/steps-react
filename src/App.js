67e574d9d35b7786cdfe2f390590dea16b350eca
Romano Ivan Pedro <romanoivanpedro@gmail.com>
2023-11-03 21:08:02 +0200

    using child props

--- src/App.js
+++ src/App.js
@@ -39,9 +39,10 @@ function Steps() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          <p className="message">
-            Step {step} : {messages[step - 1]}
-          </p>
+          {/* <p className="message">
+            <h3>Step {step}</h3> {messages[step - 1]}
+          </p> */}
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button
@@ -68,6 +69,14 @@ function Steps() {
         </div>
       )}
     </>
+  );
+}
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
